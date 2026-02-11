439d0b787aa8de6db8c661794daec3476302be53
sriram <ramanaboinasriram@gmail.com>
2026-02-11 12:56:16 +0530

    added new files

---
+++ typecasting.java
@@ -0,0 +1,7 @@
+public class typecasting{
+    public static void main (String [] args){
+        double price = 99.99;
+        int rupees = (int) price;
+        System.out.println(rupees);
+    }
+}
